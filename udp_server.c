d29e2b966def0ce0d3fa05d8e0c94e896a485f18
liuying <liu_ying1012@163.com>
2018-01-08 19:58:29 +0800

    update

--- udp_server.c
+++ udp_server.c
@@ -1,106 +1,86 @@
-    #include<stdio.h>
-    #include<stdlib.h>
-    #include<unistd.h>
-    #include<errno.h>
-    #include<sys/types.h>
-    #include<sys/stat.h>
-    #include<fcntl.h>
-    #include<sys/socket.h>
-    #include<netinet/in.h>
-    #include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
 
 
-    #define MYPORT 8887
-    #define BUFFER_SIZE 1024*16
+#define MYPORT 8887
+#define BUFFER_SIZE 1024*16
 
-    #define ERR_EXIT(m) \
-        do { \
-        perror(m); \
-        exit(EXIT_FAILURE); \
-        } while (0)
+#define ERR_EXIT(m) \
+    do { \
+    perror(m); \
+    exit(EXIT_FAILURE); \
+    } while (0)
 
-    char* strjoin(char *str1, char *str2);
+char* strjoin(char *str1, char *str2);
 
-    void echo_ser(int sock)
+void echo_ser(int sock)
+{
+    char recvbuf[BUFFER_SIZE] = {0};
+    struct sockaddr_in peeraddr;
+    socklen_t peerlen;
+    int n;
+    int fd;
+    char *filepath = "../log/";
+    char *filename = "../log/test.log";
+
+    while (1)
     {
-        char recvbuf[BUFFER_SIZE] = {0};
-        struct sockaddr_in peeraddr;
-        socklen_t peerlen;
-        int n;
-        int fd;
-	char *filepath = "../log/";
-	char *filename = "test.log";
-
-        while (1)
+        peerlen = sizeof(peeraddr);
+        memset(recvbuf, 0, sizeof(recvbuf));
+        n = recvfrom(sock, recvbuf, sizeof(recvbuf), 0, (struct sockaddr *)&peeraddr, &peerlen);
+        if (n <= 0)
         {
+            if (errno == EINTR)
+            continue;
 
-            peerlen = sizeof(peeraddr);
-            memset(recvbuf, 0, sizeof(recvbuf));
-            n = recvfrom(sock, recvbuf, sizeof(recvbuf), 0,
-                         (struct sockaddr *)&peeraddr, &peerlen);
-            if (n <= 0)
-            {
-
-                if (errno == EINTR)
-                    continue;
-
-                ERR_EXIT("recvfrom error");
-            }
-            else if(n > 0)
-            {
-                if (-1 == access(filepath, F_OK))
-                {
-                    mkdir(filepath,S_IRWXU|S_IRWXG|S_IROTH|S_IXOTH);
-                }
-                filepath = strjoin(filepath, filename);
-                fd=open(filepath,O_RDWR|O_CREAT|O_APPEND,S_IREAD|S_IWRITE);
-                if (-1 != fd)
-                {
-                    write(fd,recvbuf,n);
-                }
-                /*close(fd);*/
-                printf("Received data：%s\n",recvbuf);
-                sendto(sock, recvbuf, n, 0,
-                       (struct sockaddr *)&peeraddr, peerlen);
-                printf("Sent back data：%s\n",recvbuf);
-            }
+            ERR_EXIT("recvfrom error");
         }
-        close(sock);
+        else if(n > 0)
+        {
+            if (-1 == access(filepath, F_OK))
+            {
+                mkdir(filepath,S_IRWXU|S_IRWXG|S_IROTH|S_IXOTH);
+            }
+                
+            fd=open(filename,O_RDWR|O_CREAT|O_APPEND,S_IREAD|S_IWRITE);
+            if (-1 != fd)
+            {
+                write(fd,recvbuf,n);
+            }
+            /*close(fd);*/
+            /*printf("Received data：%s\n",recvbuf);*/
+            sendto(sock, recvbuf, n, 0, (struct sockaddr *)&peeraddr, peerlen);
+            /*printf("Sent back data：%s\n",recvbuf);*/
+        }
     }
+    close(sock);
+}
 
-    char* strjoin(char *str1, char *str2)
-    {
-        char *result = malloc(strlen(str1)+strlen(str2)+1);//+1 for the zero-terminator
-        //in real code you would check for errors in malloc here
-        if (result == NULL)
-	{
-	    printf("malloc failed!");
-	    return NULL;
-	}
+int main(void)
+{
+    int sock;
+    if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
+        ERR_EXIT("socket error");
 
-        strcpy(result, str1);
-        strcat(result, str2);
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(MYPORT);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-        return result;
-    }
+    printf("Listening port:%d\n",MYPORT);
+    if (bind(sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+        ERR_EXIT("bind error");
 
-    int main(void)
-    {
-        int sock;
-        if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
-            ERR_EXIT("socket error");
+    echo_ser(sock);
 
-        struct sockaddr_in servaddr;
-        memset(&servaddr, 0, sizeof(servaddr));
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_port = htons(MYPORT);
-        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-        printf("Listening port:%d\n",MYPORT);
-        if (bind(sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-            ERR_EXIT("bind error");
-
-        echo_ser(sock);
-
-        return 0;
-    }
+    return 0;
+}
